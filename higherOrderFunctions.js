2b34fc5d4f1638cc00c86824c8fe10e452b184ff
plimeta <plimeta@gmail.com>
2019-07-09 11:44:54 -0500

    function as arguments(1)

---
+++ higherOrderFunctions.js
@@ -0,0 +1,22 @@
+'use strict';
+
+//Function as arguments
+
+function hello() {
+  console.log('Hello world');
+}
+
+function goodbye() {
+  console.log('Goodbye world');
+}
+
+function repeat(fn, n) {
+  for (let i = 0; i <= n; i++) {
+    fn();
+  }
+}
+
+repeat(hello,5);
+
+repeat(goodbye, 5);
+
